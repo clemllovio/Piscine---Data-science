27431d4aa73e107db89d99f812c8dbeb04cdb372
clemllovio <llovio.clemence@gmail.com>
2025-03-11 15:49:10 +0100

    day02 ex02: done

--- day02/ex02/mustache.py
+++ day02/ex02/mustache.py
@@ -30,7 +30,7 @@ def get_data():
         MAX(price) AS max
         FROM customers
         WHERE event_type = 'purchase'
-        AND event_time BETWEEN '2022-10-01 00:00:00' AND '2023-01-31 23:59:59';
+        AND event_time BETWEEN '2022-10-01 00:00:00' AND '2023-02-28 23:59:59';
         """
         cursor.execute(sql_query)
         result = cursor.fetchall()
@@ -38,17 +38,17 @@ def get_data():
         sql_query = """SELECT price
         FROM customers
         WHERE event_type = 'purchase'
-        AND event_time BETWEEN '2022-10-01 00:00:00' AND '2023-01-31 23:59:59';
+        AND event_time BETWEEN '2022-10-01 00:00:00' AND '2023-02-28 23:59:59';
         """
         cursor.execute(sql_query)
         result1 = cursor.fetchall()
 
-        sql_query = """SELECT id, SUM(price) / COUNT(price) AS avg_basket_price
-FROM customers
-WHERE event_type = 'purchase'
-AND event_time BETWEEN '2022-10-01 00:00:00' AND '2023-01-31 23:59:59'
-GROUP BY id;
-                """
+        sql_query = """SELECT user_id, SUM(price) AS average_basket_price
+                        FROM customers
+                        WHERE event_type = 'purchase'
+                        AND event_time BETWEEN '2022-10-01 00:00:00' AND '2023-01-31 23:59:59'
+                        AND price > 0
+                        GROUP BY user_id;"""
         cursor.execute(sql_query)
         result2 = cursor.fetchall()
 
@@ -63,26 +63,54 @@ GROUP BY id;
 
     return result, result1, result2
 
+def plot_style():
+    plt.rcParams['axes.spines.left'] = False
+    plt.rcParams['axes.spines.right'] = False
+    plt.rcParams['axes.spines.top'] = False
+    plt.rcParams['axes.spines.bottom'] = False
+    plt.grid(axis = 'x', color='white')
+    plt.gca().set_facecolor((0.9176, 0.9176, 0.9451))
+    plt.tick_params(left=False, bottom=False)
+    plt.gca().get_yaxis().set_visible(False)
 
-def create_box_plot(price, q1, q3, average_basket_price):
-    if not price:
-        return
-    # plt.boxplot(price, vert=False, widths=0.7)
+def create_box_plot_1(prices):
+    plot_style()
+    plt.boxplot(prices, vert=False, widths=0.7, patch_artist=True, boxprops=dict(facecolor='green', color='green'), flierprops=dict(marker='D', markeredgecolor='black',markerfacecolor='grey'))
+    plt.xlabel("price")
+    plt.show()
 
-    # plt.boxplot(price, vert=False, widths=0.7, showfliers=False)
-    # print(average_basket_price)
-    plt.boxplot(average_basket_price, vert=False, widths=0.7, flierprops=dict(marker='D', markeredgecolor='black',
-                      markerfacecolor='blue'), showfliers=False)
-
+def create_box_plot_2(prices):
+    plot_style()
+    plt.boxplot(prices, vert=False, widths=0.7, showfliers=False, patch_artist=True, boxprops=dict(facecolor='green', color='green'))
+    plt.xlabel("price")
     plt.show()
 
 
+def create_box_plot_3(average_basket_price):
+    if not average_basket_price:
+        print("Error: no average basket price")
+        return
+
+    plot_style()
+    plt.boxplot(average_basket_price, vert=False, widths=0.7, showfliers=False,patch_artist=True, boxprops=dict(facecolor='blue', color='blue'))
+    plt.show()
 
 def main():
     load_dotenv(os.path.abspath("../../.env"))
-    data, price, data1 = get_data()
+    data, price, average_basket_price = get_data()
+
     count, mean, std, min, q1, q2, q3, max = zip(*data)
     print(f'count: {count}\nmean: {mean}\nstd: {std}\nmin: {min}\nq1: {q1}\nq2: {q2}\nq3: {q3}\nmax: {max}')
-    create_box_plot([p[0] for p in price], q1[0],q3[0], [d[0] for d in data1])
+
+    prices = [p[0] for p in price]
+
+    if not prices:
+        print("Error: empty prices")
+        return
+
+    create_box_plot_1(prices)
+    create_box_plot_2(prices)
+    create_box_plot_3([row[1] for row in average_basket_price])
+
 if __name__ == "__main__":
     main()
